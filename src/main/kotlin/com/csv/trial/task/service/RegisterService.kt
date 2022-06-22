ee0a3fd4459ef3e317f4666d0ed32700c6af0b3e
masahiro.yamane <masahiro.yamane@infcurion.com>
2022-06-22 16:10:18 +0900

    lamdbainvoke処理実装

--- src/main/kotlin/com/csv/trial/task/service/RegisterService.kt
+++ src/main/kotlin/com/csv/trial/task/service/RegisterService.kt
@@ -1,22 +1,22 @@
 package com.csv.trial.task.service
 
 import com.amazonaws.HttpMethod
+import com.amazonaws.services.lambda.AWSLambda
+import com.amazonaws.services.lambda.model.InvokeRequest
 import com.amazonaws.services.s3.AmazonS3
 import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest
-import com.csv.trial.task.response.TaskRegisterResponse
 import com.csv.trial.task.form.TaskRegisterForm
 import com.csv.trial.task.repository.ITaskRepository
 import com.csv.trial.task.repository.entity.Task
-import com.csv.trial.task.service.lambdaurl.req.LambdaUrlRequest
-import com.csv.trial.task.service.lambdaurl.res.LambdaUrlResponse
+import com.csv.trial.task.response.TaskRegisterResponse
 import org.springframework.stereotype.Service
-import org.springframework.web.client.RestTemplate
-import java.util.Date
+import java.util.*
 
 @Service
 class RegisterService(
         private val taskRepository: ITaskRepository,
         private val s3: AmazonS3,
+        private val lambda: AWSLambda
 ) {
     fun register(form: TaskRegisterForm): TaskRegisterResponse {
 
@@ -39,7 +39,7 @@ class RegisterService(
         val created = taskRepository.saveAndFlush(task)
 
         //LambdaURLにHTTPS通信
-        httpConnectionToLambda(taskId = task.id!!)
+        invokeLambda(taskId = task.id!!)
 
         return TaskRegisterResponse(created, preSignedUrl)
     }
@@ -48,20 +48,17 @@ class RegisterService(
     LambdaURLにHTTPS通信
     認証は一旦無視
     */
-    fun httpConnectionToLambda(taskId: Int) {
-        val restTemplate = RestTemplate()
+    fun invokeLambda(taskId: Int) {
 
-        //RequestBody
-        val lambdaUrlRequest = LambdaUrlRequest(
-                taskId = taskId
-        )
-        //URI
-        val uri = ""
+        val functionName = "myamane-lambda"
+        print(functionName)
 
-        //実行
-        val response = restTemplate.postForEntity(uri,lambdaUrlRequest, String::class.java)
+        val invokeRequest = InvokeRequest()
+                .withFunctionName(functionName)
+                .withPayload("""{"taskId":"1"}""")
 
-        //結果確認
-        println(response)
+        val response = lambda.invoke(invokeRequest)
+
+        print(response)
     }
 }
