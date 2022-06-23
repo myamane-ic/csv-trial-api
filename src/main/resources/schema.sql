4909fc1ea9d90b980b5d8a6584d28e53b0422d37
masahiro.yamane <masahiro.yamane@infcurion.com>
2022-06-23 09:57:14 +0900

    DDL修正

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,13 +1,13 @@
 --APIで登録する側
 CREATE TABLE IF NOT EXISTS task (
-    id INT auto_increment primary key,
+    id INT IDENTITY(1,1) NOT NULL,
     name VARCHAR(255) NOT NULL,
-    file_id VARCHAR(MAX)
+    file_id VARCHAR(255) NOT NULL
 );
 
 --csvで登録する側
 CREATE TABLE IF NOT EXISTS task_detail (
-    id INT auto_increment primary key,
+    id INT IDENTITY(1,1) NOT NULL,
     task_id INT NOT NULL,
-    content VARCHAR(255)
+    content VARCHAR(255) NOT NULL
 );
