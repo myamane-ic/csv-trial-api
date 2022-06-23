0a01f1f02bf13d6e1d7afc3e7f68dfa08dd73b0f
masahiro.yamane <masahiro.yamane@infcurion.com>
2022-06-23 09:58:45 +0900

    Revert "DDL修正"
    This reverts commit 4909fc1ea9d90b980b5d8a6584d28e53b0422d37.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,13 +1,13 @@
 --APIで登録する側
 CREATE TABLE IF NOT EXISTS task (
-    id INT IDENTITY(1,1) NOT NULL,
+    id INT auto_increment primary key,
     name VARCHAR(255) NOT NULL,
-    file_id VARCHAR(255) NOT NULL
+    file_id VARCHAR(MAX)
 );
 
 --csvで登録する側
 CREATE TABLE IF NOT EXISTS task_detail (
-    id INT IDENTITY(1,1) NOT NULL,
+    id INT auto_increment primary key,
     task_id INT NOT NULL,
-    content VARCHAR(255) NOT NULL
+    content VARCHAR(255)
 );
